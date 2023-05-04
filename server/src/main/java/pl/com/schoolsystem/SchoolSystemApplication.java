d83a1c880aec833c86aa45e496f93ec01ab3aed5
Mateusz Grodzki <mateusz.maria.grodzki@gmail.com>
2023-05-04 12:47:19 +0200

    create docker configuration for docker compose

--- server/src/main/java/pl/com/schoolsystem/SchoolSystemApplication.java
+++ server/src/main/java/pl/com/schoolsystem/SchoolSystemApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SchoolsystemApplication {
+public class SchoolSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SchoolsystemApplication.class, args);
+		SpringApplication.run(SchoolSystemApplication.class, args);
 	}
 
 }
