f201a68b2bd8424fb6b9494a631b769e996db017
Mateusz Grodzki <mateusz.maria.grodzki@gmail.com>
2023-05-10 08:57:23 +0200

    deleted unneded comma on last line in sql migration

--- server/src/main/resources/db/migration/V202305091324__create_application_user_table.sql
+++ server/src/main/resources/db/migration/V202305091324__create_application_user_table.sql
@@ -5,5 +5,5 @@ CREATE TABLE IF NOT EXISTS application_user(
     phone_number VARCHAR(32) NOT NULL,
     email VARCHAR(256) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    role VARCHAR(32) NOT NULL,
+    role VARCHAR(32) NOT NULL
 )
