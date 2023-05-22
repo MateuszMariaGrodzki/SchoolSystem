5ed9691d0fc30100b8a1157e88bbbac9f24cedcb
Mateusz Grodzki <mateusz.maria.grodzki@gmail.com>
2023-05-22 10:19:10 +0200

    add another tests

--- server/src/test/java/pl/com/schoolsystem/security/user/ApplicationUserServiceTest.java
+++ server/src/test/java/pl/com/schoolsystem/security/user/ApplicationUserServiceTest.java
@@ -6,10 +6,12 @@ import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.junit.Assert.assertThrows;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
+import static pl.com.schoolsystem.security.user.ApplicationRole.ADMIN;
 
 import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import pl.com.schoolsystem.common.exception.ApplicationUserNotFoundException;
+import pl.com.schoolsystem.common.exception.DuplicatedApplicationUserEmailException;
 
 public class ApplicationUserServiceTest {
 
@@ -49,5 +51,28 @@ public class ApplicationUserServiceTest {
                 applicationUserService.getByEmailsOrElseThrowApplicationUserNotFoundException(
                     email))
         .doesNotThrowAnyException();
+  }
+
+  @Test
+  void shouldThrowExceptionWhenEmailAlreadyExistsInDatabase() {
+    // given
+    final var command =
+        new AddApplicationUserCommand(
+            "Admin",
+            "Adminowski",
+            "6587412",
+            "qwerty@onet.pl",
+            "dsadasgjdkjghsfgfdkgbfdkgd",
+            ADMIN);
+    given(applicationUserRepository.existsByEmail(command.email())).willReturn(true);
+    // when
+    final var exception =
+        assertThrows(
+            DuplicatedApplicationUserEmailException.class,
+            () -> applicationUserService.create(command));
+    // then
+    assertThat(exception.getCode()).isEqualTo("DUPLICATED_EMAIL");
+    assertThat(exception.getDisplayMessage())
+        .isEqualTo("Email: qwerty@onet.pl already exists in system");
   }
 }
