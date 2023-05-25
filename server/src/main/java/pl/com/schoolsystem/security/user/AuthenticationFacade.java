c79f9660a006a415a179ffe0f5d19293a457950c
Mateusz Grodzki <mateusz.maria.grodzki@gmail.com>
2023-05-25 07:29:06 +0200

    add mapping to principal before casting to application user

--- server/src/main/java/pl/com/schoolsystem/security/user/AuthenticationFacade.java
+++ server/src/main/java/pl/com/schoolsystem/security/user/AuthenticationFacade.java
@@ -1,6 +1,7 @@
 package pl.com.schoolsystem.security.user;
 
 import java.util.Optional;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import pl.com.schoolsystem.common.exception.UnexpectedBehaviourException;
@@ -10,6 +11,7 @@ public class AuthenticationFacade {
 
   public ApplicationUserEntity getAuthenticatedUser() {
     return Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication())
+        .map(Authentication::getPrincipal)
         .filter(ApplicationUserEntity.class::isInstance)
         .map(ApplicationUserEntity.class::cast)
         .orElseThrow(
