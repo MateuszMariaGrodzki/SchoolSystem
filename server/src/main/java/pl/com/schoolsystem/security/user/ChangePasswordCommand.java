cb80783ad862c151f700766066a5ca2604c8db6c
Mateusz Grodzki <mateusz.maria.grodzki@gmail.com>
2023-05-25 07:30:38 +0200

    add additional password validation

--- server/src/main/java/pl/com/schoolsystem/security/user/ChangePasswordCommand.java
+++ server/src/main/java/pl/com/schoolsystem/security/user/ChangePasswordCommand.java
@@ -1,8 +1,13 @@
 package pl.com.schoolsystem.security.user;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public record ChangePasswordCommand(
-    @NotBlank String oldPassword,
-    @NotBlank String newPassword,
-    @NotBlank String retypedNewPassword) {}
+    @NotBlank(message = "old password is mandatory") String oldPassword,
+    @NotBlank(message = "new password is mandatory")
+        @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\\\d)(?=.*[@$!%*?&])[A-Za-z\\\\d@$!%*?&]{8,}$",
+            message = "Password too weak")
+        String newPassword,
+    @NotBlank(message = "retyped new password is mandatory") String retypedNewPassword) {}
