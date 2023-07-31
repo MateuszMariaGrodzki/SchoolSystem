feb10e3ff66722686d08d1adb27e8e2c10a57b9a
Mateusz Grodzki <mateusz.grodzki@britenet.com.pl>
2023-07-31 14:31:09 +0200

    deleted public modyfier in StudentRepository

--- server/src/main/java/pl/com/schoolsystem/student/StudentRepository.java
+++ server/src/main/java/pl/com/schoolsystem/student/StudentRepository.java
@@ -2,4 +2,4 @@ package pl.com.schoolsystem.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StudentRepository extends JpaRepository<StudentEntity, Long> {}
+interface StudentRepository extends JpaRepository<StudentEntity, Long> {}
