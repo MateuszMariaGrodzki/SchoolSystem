853d701c6958200f68489bdd7c0541e755912ad9
Mateusz Grodzki <mateusz.grodzki@britenet.com.pl>
2023-07-31 14:23:21 +0200

    add missing @PostMapping annotation

--- server/src/main/java/pl/com/schoolsystem/student/api/StudentController.java
+++ server/src/main/java/pl/com/schoolsystem/student/api/StudentController.java
@@ -5,10 +5,7 @@ import static org.springframework.http.HttpStatus.CREATED;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.com.schoolsystem.student.StudentCommand;
 import pl.com.schoolsystem.student.StudentService;
 import pl.com.schoolsystem.student.StudentView;
@@ -22,6 +19,7 @@ public class StudentController {
   private final StudentService studentService;
 
   @ResponseStatus(CREATED)
+  @PostMapping
   public StudentView create(@RequestBody @Valid StudentCommand command) {
     return studentService.create(command);
   }
