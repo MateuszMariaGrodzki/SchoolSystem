74cb5eab693dc055ccf7fbc87661a28b37c4084e
Mateusz Grodzki <mateusz.grodzki@britenet.com.pl>
2023-08-09 14:41:57 +0200

    moved generate password method to password service

--- server/src/main/java/pl/com/schoolsystem/teacher/TeacherService.java
+++ server/src/main/java/pl/com/schoolsystem/teacher/TeacherService.java
@@ -2,7 +2,6 @@ package pl.com.schoolsystem.teacher;
 
 import static pl.com.schoolsystem.security.user.ApplicationRole.TEACHER;
 import static pl.com.schoolsystem.security.user.ApplicationUserMapper.APPLICATION_USER_MAPPER;
-import static pl.com.schoolsystem.security.user.PasswordGenerator.generatePassword;
 import static pl.com.schoolsystem.teacher.TeacherMapper.TEACHER_MAPPER;
 
 import lombok.RequiredArgsConstructor;
@@ -30,7 +29,7 @@ public class TeacherService {
 
   @Transactional
   public TeacherView create(TeacherCommand teacherCommand) {
-    final var password = generatePassword();
+    final var password = passwordService.generateNewRandomPassword();
     final var applicationUserCommand =
         APPLICATION_USER_MAPPER.toApplicationUserCommand(
             teacherCommand.personalData(), passwordService.encodePassword(password), TEACHER);
