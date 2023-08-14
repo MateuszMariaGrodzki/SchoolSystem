96c6ae4e477904f704cfa9e00551a2f86b8adc79
Mateusz Grodzki <mateusz.grodzki@britenet.com.pl>
2023-08-14 15:35:22 +0200

    fix regular expression

--- server/src/main/java/pl/com/schoolsystem/school/AddressCommand.java
+++ server/src/main/java/pl/com/schoolsystem/school/AddressCommand.java
@@ -6,5 +6,5 @@ import jakarta.validation.constraints.Pattern;
 public record AddressCommand(
     @NotBlank(message = "city is mandatory") String city,
     @NotBlank(message = "street is mandatory") String street,
-    @Pattern(regexp = "//d{3}-//d{2}", message = "Post code has bad pattern") String postCode,
+    @Pattern(regexp = "\\d{2}-\\d{3}", message = "Post code has bad pattern") String postCode,
     @NotBlank(message = "building is mandatory") String building) {}
