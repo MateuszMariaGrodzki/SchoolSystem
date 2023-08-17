c5566e9a74593a0d03439f228e14c3570e38f89d
MateuszMariaGrodzki <50069187+MateuszMariaGrodzki@users.noreply.github.com>
2023-08-17 19:40:27 +0200

    refactored update method (#47)

---
+++ server/src/test/java/pl/com/schoolsystem/school/SchoolServiceDataTestFactory.java
@@ -0,0 +1,27 @@
+package pl.com.schoolsystem.school;
+
+import static pl.com.schoolsystem.school.SchoolLevel.PRIMARY;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class SchoolServiceDataTestFactory {
+
+  public SchoolEntity provideSchoolEntityForPostMethod(long id) {
+    final var entity = new SchoolEntity();
+    entity.setId(id);
+    entity.setName("Liceum");
+    entity.setTier(PRIMARY);
+    entity.setAddress(provideAddressEmbeddableForPostMethod());
+    return entity;
+  }
+
+  public AddressEmbeddable provideAddressEmbeddableForPostMethod() {
+    final var address = new AddressEmbeddable();
+    address.setCity("Lublin");
+    address.setPostCode("80-666");
+    address.setBuilding("70");
+    address.setStreet("lubelskiego lipca");
+    return address;
+  }
+}
