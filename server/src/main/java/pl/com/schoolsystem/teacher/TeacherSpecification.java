32982fd2436808eeb61f58b89158e3db2b919d17
Mateusz Grodzki <mateusz.grodzki@britenet.com.pl>
2023-08-30 12:49:45 +0200

    created specifications or administrator, teacher and student

---
+++ server/src/main/java/pl/com/schoolsystem/teacher/TeacherSpecification.java
@@ -0,0 +1,20 @@
+package pl.com.schoolsystem.teacher;
+
+import lombok.experimental.UtilityClass;
+import org.springframework.data.jpa.domain.Specification;
+import pl.com.schoolsystem.security.user.ApplicationUserEntity_;
+
+@UtilityClass
+public class TeacherSpecification {
+
+  public Specification<TeacherEntity> withId(long id) {
+    return (root, query, cb) -> cb.equal(root.get(TeacherEntity_.id), id);
+  }
+
+  public Specification<TeacherEntity> isAccountActive() {
+    return (root, query, cb) -> {
+      final var applicationUser = root.get(TeacherEntity_.applicationUser);
+      return cb.equal(applicationUser.get(ApplicationUserEntity_.isExpired), false);
+    };
+  }
+}
